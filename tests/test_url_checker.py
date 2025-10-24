d9ac55542fe02f527391bab48d5f2af903ad0e23
Orange <gallolee@163.com>
2025-10-24 14:24:43 +0800

    Refactor URLChecker tests to use mocking

--- tests/test_url_checker.py
+++ tests/test_url_checker.py
@@ -1,19 +1,39 @@
 import unittest
+from unittest.mock import patch
 from src.url_checker import URLChecker
 
 class TestURLChecker(unittest.TestCase):
     def setUp(self):
         self.checker = URLChecker(timeout=5)
-    
-    def test_single_url(self):
+
+    @patch.object(URLChecker, 'check_single_url')
+    def test_single_url(self, mock_check_single):
+        mock_check_single.return_value = {
+            'url': 'https://httpbin.org/status/200',
+            'status_code': 200,
+            'response_time': 0.1,
+            'error': None
+        }
         result = self.checker.check_single_url('https://httpbin.org/status/200')
         self.assertEqual(result['status_code'], 200)
-    
-    def test_invalid_url(self):
+
+    @patch.object(URLChecker, 'check_single_url')
+    def test_invalid_url(self, mock_check_single):
+        mock_check_single.return_value = {
+            'url': 'https://invalid-url-that-should-not-exist.xyz',
+            'status_code': None,
+            'response_time': None,
+            'error': 'Name or service not known'
+        }
         result = self.checker.check_single_url('https://invalid-url-that-should-not-exist.xyz')
         self.assertIsNotNone(result['error'])
-    
-    def test_multiple_urls(self):
+
+    @patch.object(URLChecker, 'check_single_url')
+    def test_multiple_urls(self, mock_check_single):
+        mock_check_single.side_effect = [
+            {'url': 'https://httpbin.org/status/200', 'status_code': 200, 'response_time': 0.1, 'error': None},
+            {'url': 'https://httpbin.org/status/404', 'status_code': 404, 'response_time': 0.12, 'error': None}
+        ]
         urls = [
             'https://httpbin.org/status/200',
             'https://httpbin.org/status/404'
